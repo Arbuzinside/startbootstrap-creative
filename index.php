1c8a97ac5215b7d354e90ddf301146cd293dd965
Dima Arbuzin <arbuzinside@gmail.com>
2015-05-23 14:28:35 +0300

    email

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php include_once("home.html"); ?>
+
+<html>
+<body>
+
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+Thanks for the interest. We will reach you: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
