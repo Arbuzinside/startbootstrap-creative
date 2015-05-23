fabefad6adbc339977682d32654e710dde3cee81
Dima Arbuzin <arbuzinside@gmail.com>
2015-05-23 16:33:04 +0300

    mailing test

--- welcome.php
+++ welcome.php
@@ -1,7 +1,18 @@
-<html>
-<body>
+// Here we get all the information from the fields sent over by the form.
 
-Thanks for the interest. We will reach you: <?php echo $_POST["email"]; ?>
+$email = $_POST['email'];
 
-</body>
-</html>
+$name = 'New user';
+$message = 'One more click on your website';
+ 
+$to = 'moneyjar@moneyjar.ml';
+$subject = 'Woop! New costumer interested!';
+$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
+$headers = 'From: moneyjarl@moneyjar.ml' . "\r\n";
+ 
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
+mail($to, $subject, $message, $headers); //This method sends the mail.
+echo "Your email was sent!"; // success message
+}else{
+echo "Invalid Email, please provide an correct email.";
+}
